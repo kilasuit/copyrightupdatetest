95864166a49c5cbcfd065a217450a78e617b2bed
Ryan Yates <ryan.Yates@kilasuit.org>
2025-03-17 05:32:54 +0000

    minor update

--- updateCopyrightYearViaPR.ps1
+++ updateCopyrightYearViaPR.ps1
@@ -14,6 +14,7 @@ git config --global user.name 'github-actions[bot]'
 git config --global user.email 'github-actions[bot]@users.noreply.github.com'
 git add config.toml
 git commit -m "Update copyright notice for the year $currentYear"
-git push --set-upstream origin update-copyright-year-2025
+git push --set-upstream origin update-copyright-year-2025 --force
 
+## This works locally, but I can't get it to work in the GitHub Actions workflow atm
 gh pr create --title "Update copyright notice for the year $currentYear" --body "This PR updates the copyright notice for the year $currentYear" --base main --head $branchName --reviewer kilasuit --label "enhancement" --assignee kilasuit --project 'Sample Project'
